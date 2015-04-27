818bd32632aa4440499ad5a1b0ab0e0f52e007d2
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-27 19:24:24 +0000

    wordpress 4.2
    git-svn-id: https://plugins.svn.wordpress.org/google-maps-master/tags/4.4.1.5@1147488 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/google-maps-master-admin-addons-table.php
@@ -0,0 +1,79 @@
+<?php
+if(!class_exists('WP_List_Table')){
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+class google_maps_master_admin_addons_table extends WP_List_Table {
+	/**
+	 * Display the rows of records in the table
+	 * @return string, echo the markup of the rows
+	 */
+	function display() {
+?>
+<table class="widefat fixed" cellspacing="0">
+	<thead>
+		<tr>
+			<th id="columnname" class="manage-column column-columnname" scope="col" width="300"><legend><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Screenshot', 'google_maps_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Description', 'google_maps_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Installed', 'google_maps_master'); ?></h3></legend></th>
+		</tr>
+	</thead>
+
+	<tfoot>
+		<tr>
+			<th class="manage-column column-columnname" scope="col" width="300"><a class="button-primary" href="http://wordpress.techgasp.com/google-maps-master/" target="_blank" title="Get Add-ons" style="float:left;">Get Add-ons</a></p></th>
+			<th class="manage-column column-columnname" scope="col"></th>
+			<th class="manage-column column-columnname" scope="col"><a class="button-primary" href="http://wordpress.techgasp.com/google-maps-master/" target="_blank" title="Get Add-ons" style="float:right;">Get Add-ons</a></p></th>
+		</tr>
+	</tfoot>
+
+	<tbody>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-widget-viral.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Google Maps Master Viral Widget</h3><p>The Widget that turns your Wordpress "virulent" by allowing people to +1 and Share your pages. Watch those visits explode and greatly boost your Google SEO Rank!</p><p>Looks great when published under any maps widget, remember you can always hide the widget title to get the button closer to the below widgets.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-yes.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-widget-basic.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Google Maps Master Basic Widget</h3><p>This Widget was specially designed for extremely fast page load times and small system trace. Google Maps Master Basic Widget is the professional heavy duty widget to display your business location.</p><p>Built with html5 and Google API v3 for fast page load times and perfect Google SEO. No Google Maps API key needed.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-yes.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-widget-advanced.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Google Maps Master Advanced Widget</h3><p>This is the "top of the line" business maps for Wordpress. <b>Fully Mobile Responsive</b>, this widget was specially designed to grant you full control over map options. Google Maps Master Advanced Widget allows up to 12 Markers in a single map, 10 TechGasp Designed Marker Icons and 11 customizable fields per Marker (Displays Business or Personal Logo, Name, Address, City, State, Country, Phone, Mobile, Email, Website,and Free Text Field). No Google Maps API key needed.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-widget-information.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Google Maps Master Information Widget</h3><p>The perfect widget to display the latest google maps V3 traffic, transit, weather and bicycling map informations with incredibly beautiful design. Includes 4 Information Overlays for fast and easy deployments (<b>Traffic</b>, <b>Transit</b>, <b>Bicycling</b> and <b>Incredible Weather Forecast</b>). The Weather Information contains accurate weather forecasts if you click the forecast icon, according to the zoom level of the map, it will also display real time cloud overlays. OMG!!!</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-widget-radioactive.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Google Maps Master Radioactive Widget</h3><p>This widget was specially designed to create incredibly beautiful and original Google Maps. Includes 4 TechGasp Designed Google Maps Overlays for fast and easy deployments (<b>Before Doomsday</b>, <b>Green Radiation</b>, <b>Blue Aftermath</b>, <b>Red Dawn</b>, <b>Orange Mushroom</b> and <b>Black Fallout</b>). It's also packed with 2 easy options so you can create your own customized Google Map Overlay. Awesome!!!</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-shortcode-in.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Individual Shortcode</h3><p>Google Maps Master uses TechGasp Wordpress Framework. The <b>Individual Shortcode</b> allows you to have a different customized shortcode in each page or post. Easy to use it can be found when you edit a page or a post under the wordpress text editor. Once you have created your shortcode, Just insert the shortcode <b>[google-maps-master]</b> anywhere inside that text.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-shortcode-un.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Universal Shortcode</h3><p>Google Maps Master uses TechGasp Wordpress Framework. The <b>Universal Shortcode</b> allows you to have the same shortcode across different pages or posts. Easy to use it can be found right here under the Shortcodes menu. Once you have created your shortcode, Just insert the shortcode <b>[google-maps-master-un]</b> anywhere inside the text of your pages or posts.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-updater.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Advanced Updater</h3><p>The Advanced Updater allows you to easily Update / Upgrade your Advanced Plugin. You can instantly update your plugin after we release a new version with more goodies without having to wait for the nightly native wordpress updater that runs every 24/48 hours. Get it fresh, get it fast.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-googlemapsmaster-admin-addons-support.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Award Winning Professional Support</h3><p>Need professional help deploying this plugin? TechGasp provides 24/7 award winning professional wordpress support for all advanced version costumers and replies to support tickets usually within minutes of being received. Support Us and we will Support You.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('google_maps_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+	</tbody>
+</table>
+<?php
+		}
+}
